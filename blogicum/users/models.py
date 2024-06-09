231e8addde2eba926dceed044ed1d3afdf123ee9
Andykrkk <107392133+Andykrkk@users.noreply.github.com>
2024-06-09 09:41:48 +0300

    Delete blogicum directory

--- blogicum/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.urls import reverse
-
-
-class MyUser(AbstractUser):
-
-    def get_absolute_url(self):
-        # С помощью функции reverse() возвращаем URL объекта.
-        return reverse('blog:profile', kwargs={'slug': self.slug})
